bfb62fe2b58caf753fe8f03f84c202f339e2b599
Shubham Pandey <57441082+shubham-jr@users.noreply.github.com>
2022-12-17 08:50:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // const dotenv = require("dotenv");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 // dotenv.config({ path: "./env/config.env" });
 
 console.log(process.env);
@@ -7,18 +7,18 @@ console.log(process.env);
 const app = require("./app");
 const port = process.env.PORT || 3333;
 
-const DB = process.env.DATABASE.replace(
-  "<password>",
-  process.env.DATABASE_PASSWORD
-);
-mongoose
-  .connect(DB)
-  .then(() => {
-    console.log("database connected....");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// const DB = process.env.DATABASE.replace(
+//   "<password>",
+//   process.env.DATABASE_PASSWORD
+// );
+// mongoose
+//   .connect(DB)
+//   .then(() => {
+//     console.log("database connected....");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
 app.get("/", (req, res, next) => {
   res.status(200).json({
